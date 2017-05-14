5267204152c2f17df5849a16b9b47fb8b170b21e
Danang P. Kalingga <juslemon@outlook.com>
2017-05-14 21:41:53 +0700

    Final

---
+++ app/src/main/java/id/sch/smktelkom_mlg/privateassignment/xirpl509/cinemain/Service/API.java
@@ -0,0 +1,6 @@
+package id.sch.smktelkom_mlg.privateassignment.xirpl509.cinemain.Service;
+
+public class API {
+    static public String API_URL = "http://api.themoviedb.org/3/discover/movie";
+    static public String API_KEY = "5001c7e284c197dd80b5ac88bf6b3707";
+}
